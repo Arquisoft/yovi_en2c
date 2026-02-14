026617d45c4de5e40810e33ea46a3dc7dc20a613
Ana294100 <uo294100@uniovi.es>
2026-02-14 18:08:59 +0100

    Fixed more issues

--- gatewayservice/vitest.config.js
+++ gatewayservice/vitest.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['lcov'],
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
       reportsDirectory: './coverage'
     }
   }
