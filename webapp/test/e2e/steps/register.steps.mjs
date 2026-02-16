b96b227311ddcd999a9cbd5d9f421aadbd2493d0
Ana294100 <uo294100@uniovi.es>
2026-02-16 11:45:59 +0100

    A detail

--- webapp/test/e2e/steps/register.steps.mjs
+++ webapp/test/e2e/steps/register.steps.mjs
@@ -13,7 +13,7 @@ When('I enter {string} as the username and submit', async function (username) {
   await page.click('.submit-button')
 })
 
-Then('I should be redirected to the game page', async function (expected) {
+Then('I should be redirected to the game page', async function () {
   const page = this.page
   if (!page) throw new Error('Page not initialized')
 
